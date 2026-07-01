e4ed9826bde481491370ad62c235b93f34a8c974
chostanzz <dewianyar21@gmail.com>
2026-07-01 18:14:29 +0700

    fix main

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"RPL/routes"
 	"RPL/utils"
 	"net/http"
+	"os"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/labstack/echo/v4"
@@ -17,16 +18,24 @@ func main() {
 		Validator: validator.New(),
 	}
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:5173"},
-		// Izinkan metode HTTP yang diperlukan
-		AllowMethods: []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
-		// PENTING: Izinkan header Authorization agar token JWT dari Axios bisa masuk
-		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowOrigins: []string{
+			"http://localhost:5173",
+			"https://dentallab.up.railway.app",
+		},
+		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete, http.MethodOptions},
+		AllowHeaders:     []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+		AllowCredentials: true,
 	}))
 
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	e.Logger.Fatal(e.Start(":" + port))
 	// hash, _ := bcrypt.GenerateFromPassword(
 	// 	[]byte("kucinghitam"),
 	// 	bcrypt.DefaultCost,
@@ -35,5 +44,5 @@ func main() {
 	// fmt.Println(
 	// 	base64.StdEncoding.EncodeToString(hash),
 	// )
-	e.Logger.Fatal(e.Start(":8080"))
+	// e.Logger.Fatal(e.Start(":8080"))
 }
